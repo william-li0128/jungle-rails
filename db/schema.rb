74db92c36383c7df7f7780bce094e26d9864b5b3
William Li <110069721+william-li0128@users.noreply.github.com>
2023-03-14 21:24:31 -0700

    implement feature of admin/categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_13_235634) do
+ActiveRecord::Schema.define(version: 2023_03_14_224645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abouts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
